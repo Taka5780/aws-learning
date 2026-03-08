970471d7cc695c0782ac36c96f53f8e36f465574
Taka5780 <papahashi0913@gmail.com>
2026-03-09 02:06:12 +0900

    Modifications to Terraform and Addition of Ansible

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,16 +1,16 @@
 # make VPC
 resource "aws_vpc" "dev_vpc" {
-    cidr_block = "10.0.0.0/28"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "terraform-study-vpc"
-    }
+  tags = {
+    Name = "terraform-study-vpc"
+  }
 }
 
 # make subnet
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.dev_vpc.id
-  cidr_block = "10.0.0.0/28"
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -20,7 +20,7 @@ resource "aws_subnet" "public" {
 
 # internet gw
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.dev_vpc.id
+  vpc_id = aws_vpc.dev_vpc.id
 }
 
 # route table
@@ -35,6 +35,6 @@ resource "aws_route_table" "public" {
 
 # Subnet and Route Table Association
 resource "aws_route_table_association" "public" {
-    subnet_id = aws_subnet.public.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
